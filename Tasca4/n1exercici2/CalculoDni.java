0dd1174d65d4229574a2e9ee3b984cc967df8689
JuanmaMesa <juanma_sme@hotmail.com>
2023-10-09 18:39:28 +0200

    app dni completa

--- Tasca4/n1exercici2/CalculoDni.java
+++ Tasca4/n1exercici2/CalculoDni.java
@@ -1,14 +1,26 @@
 package n1exercici2;
-
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class CalculoDni {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Dime tu digitos del Dni");
+		try {
+		int numDni = input.nextInt();
+		System.out.println("tu letra del DNI es: "+numDni+"-"+calcularLetra(numDni));
+		
+		}catch(InputMismatchException e) {
+			System.out.println("InputMismatchException: Error! No has puesto tus digitos.");
+		}
 
 	}
-	public String calcularLetra() {
-		
-		return "";
+	public static char calcularLetra(int num) {
+		num = (num %23);
+		final String letter = "TRWAGMYFPDXBNJZSQVHLCKE";
+		char letterDni = letter.charAt(num);
+		return letterDni;
 	}
 
 
