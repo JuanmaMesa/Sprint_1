b799af9d0f6b2bbdfa8532f790a91789fab3191f
JuanmaMesa <juanma_sme@hotmail.com>
2023-10-21 09:00:40 +0200

    ejecutando con exito primer lambda

---
+++ Tasca8_Lambdas/n1exercici1/App_lambda.java
@@ -0,0 +1,28 @@
+package n1exercici1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class App_lambda {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> names = new ArrayList<>(Arrays.asList("John","mouse","number","paper","Orlando","Sunday"));
+		
+		List<String> filteredNames = filterStrings(names);
+		
+		filteredNames.forEach(s -> System.out.println(s));
+				
+	}
+
+	public static List<String> filterStrings(ArrayList<String>strings){
+		
+		return strings.stream()
+				.filter((s) -> s.toLowerCase().contains("o"))
+				.collect(Collectors.toList());
+			
+					
+	}
+}
