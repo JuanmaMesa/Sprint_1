e66d372144646c19b256f0f22c18f2b4e68b000b
JuanmaMesa <juanma_sme@hotmail.com>
2023-10-22 20:49:10 +0200

    empezando

---
+++ Tasca8_Lambdas/n1exercici8/App.java
@@ -0,0 +1,21 @@
+package n1exercici8;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		
+	
+
+	}
+	
+	@FunctionalInterface
+	interface  Reverse {
+		String reverse(String str);
+		
+		
+		
+		
+	}
+
+}
