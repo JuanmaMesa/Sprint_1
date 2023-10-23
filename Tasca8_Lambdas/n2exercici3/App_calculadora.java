4825595e6915711b040d89438efd62156a63e6c3
JuanmaMesa <juanma_sme@hotmail.com>
2023-10-23 21:12:53 +0200

    resisando

--- Tasca8_Lambdas/n2exercici3/App_calculadora.java
+++ Tasca8_Lambdas/n2exercici3/App_calculadora.java
@@ -9,7 +9,7 @@ public class App_calculadora {
 		Calculator addiction = (a,b,c) -> a + b + c;
 		Calculator subtract = (a,b,c) -> (b - c);
 		Calculator multiply = (a,b,c) -> (a * c) *b;
-		Calculator division = (a,b,c) -> (a / c);
+		Calculator division = (a,b) -> ;
 		
 		System.out.println("suma -> "+addiction.operations(5, 5, 6));
 		System.out.println("resta -> "+subtract.operations(5, 6, 6));
