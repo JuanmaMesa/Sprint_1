50fa412847e2b6c45d8f3663af27202e204f6f0e
Alex Pohl <Alex Pohl>
2023-10-23 12:47:29 +0200

    finalizado, falta revision

---
+++ Tasca8_Lambdas/n2exercici3/App_calculadora.java
@@ -0,0 +1,39 @@
+package n2exercici3;
+
+import javax.management.OperationsException;
+
+public class App_calculadora {
+
+	public static void main(String[] args) {
+		
+		Calculator addiction = (a,b,c) -> a + b + c;
+		Calculator subtract = (a,b,c) -> (b - c);
+		Calculator multiply = (a,b,c) -> (a * c) *b;
+		Calculator division = (a,b,c) -> (a / c);
+		
+		System.out.println("suma -> "+addiction.operations(5, 5, 6));
+		System.out.println("resta -> "+subtract.operations(5, 6, 6));
+		System.out.println("multiplicacion -> "+multiply.operations(5, 5, 6.9f));
+		System.out.println("division -> "+division.operations(5, 5, 6));
+	
+		
+		
+		System.out.println("division -> "+ String.format("%.2f", division.operations(5, 5, 6)));     //+division.operations(5, 5, 6));
+		
+		
+		
+		
+		
+		
+
+	}
+	
+	@FunctionalInterface
+	interface Calculator{
+		float operations(float a, float b, float c);
+		
+	}
+	
+	
+
+}
---
+++ Tasca8_Lambdas/n2exercici4/App.java
@@ -0,0 +1,22 @@
+package n2exercici4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+
+public class App {
+
+	public static void main(String[] args) {
+		ArrayList<Object> Lista = new ArrayList<Object>(Arrays.asList(1,"Marta","Numero", 22,"Oscar"));
+		
+		
+		
+	}
+	
+	
+		
+		
+	
+
+}
