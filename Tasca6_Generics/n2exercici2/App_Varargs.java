c64a61b374684495cd3dab6f13659ba608fa9986
Alex Pohl <Alex Pohl>
2023-10-23 09:26:09 +0200

    comprobacion hecha

--- Tasca6_Generics/n2exercici2/App_Varargs.java
+++ Tasca6_Generics/n2exercici2/App_Varargs.java
@@ -5,10 +5,10 @@ public class App_Varargs {
 	public static void main(String[] args) {
 		GenericMethods prueba = new GenericMethods();
 		
+		System.out.println("--Comprobacion numero indefinido de parametros--\n");
 		prueba.printGenericArguments("Marcos",32,"Barcelona");
 		
-		System.out.println("--- Segunda prueba ---");
-		
+		System.out.println("--- Segundo test ---");
 		prueba.printGenericArguments(new Persona(),true,"Pilar",42,3.5);
 
 	}
