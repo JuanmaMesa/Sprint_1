2c5901166029b974e970e37309f75ab2ee8e5212
JuanmaMesa <juanma_sme@hotmail.com>
2023-10-24 21:00:02 +0200

    finalizado

--- Tasca8_Lambdas/n2exercici4/App.java
+++ Tasca8_Lambdas/n2exercici4/App.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.Comparator;
 
 
@@ -11,24 +12,53 @@ import java.util.Comparator;
 public class App {
 
 	public static void main(String[] args) {
-		ArrayList<Object> lista = new ArrayList<Object>(Arrays.asList(1,"Marta","Numero", 22,"Oscar"));
-		ArrayList<String> strings = new ArrayList<String>(Arrays.asList("Hola", "Adios"));
-		//Collections.sort((List<Object>) lista);
+		ArrayList<Object> objectsArray = new ArrayList<Object>(Arrays.asList(1,"Marta","Numero", 22,"Oscar", "Alma", "Berto"));
+		ArrayList<String> stringsList = new ArrayList<String>();
+		
+		for(Object obj: objectsArray) {
+			
+			if(obj instanceof String) {
+				
+				stringsList.add((String)obj);
+			}
+		}
+		
+		List<String> orderAlphabetic = stringsList.stream()
+				.sorted((s1, s2) -> Integer.compare(s1.charAt(0), s2.charAt(0)))
+				.collect(Collectors.toList());
+		
+		System.out.println(orderAlphabetic);
 		
 		
-		strings.forEach(n -> System.out.println(n));
+		
+		
+		List<String> letterE = stringsList.stream()
+				.sorted((s1, s2) ->{
+					boolean e1 = s1.contains("e");
+					boolean e2 = s2.contains("e");
+					return Boolean.compare(e2,e1);
+					})
+				.collect(Collectors.toList());
+		
+		System.out.println(letterE);
+		
+		
+		List<String> changeLetterA = stringsList.stream()
+				.map( s -> s.replaceAll("a", "4"))
+				.collect(Collectors.toList());
+		
+		System.out.println(changeLetterA);
+		
+		List<Object> onlyNumbers = objectsArray.stream()
+				.filter( n  -> String.valueOf(n).matches("-?\\d+(\\.\\d+)?"))
+				.collect(Collectors.toList());
+		
+		System.out.println(onlyNumbers);
+		
+		
 		
 	}
 
-	@Override
-	public int compare(String s1, String s2) {
-		
-		int name = r1
-				
-				
-		
-		
-	}
 	
 	
 	
