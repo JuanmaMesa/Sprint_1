993934af8e7a2dd71a76d4d1521734a403c5407a
Alex Pohl <Alex Pohl>
2023-10-24 09:35:47 +0200

    Finalizado

--- Laboratorio/lab2/Inverse.java
+++ Laboratorio/lab2/Inverse.java
@@ -3,22 +3,15 @@ package lab2;
 public class Inverse {
 	
 	public static void main(String[]arhs) {
-		
-		int numeros[] = {1,2,3,4,5};
-		
-		//for(int n : numeros) {
-			//System.out.println(n);
-		//}
-		
-		for (int indice = numeros.length -1; indice >= 0;indice--) {
-			
-			System.out.println(numeros[indice]);
-			
-		}
-		
-		
-		
-	}
 	
+		float a = 5;
+		float b = 0;
+	
+		float numero = a / b;
+		System.out.println(numero);
 
+		
+		
+		
+}
 }
--- Tasca8_Lambdas/n2exercici3/App_calculadora.java
+++ Tasca8_Lambdas/n2exercici3/App_calculadora.java
@@ -6,32 +6,29 @@ public class App_calculadora {
 
 	public static void main(String[] args) {
 		
-		Calculator addiction = (a,b,c) -> a + b + c;
-		Calculator subtract = (a,b,c) -> (b - c);
-		Calculator multiply = (a,b,c) -> (a * c) *b;
-		Calculator division = (a,b) -> ;
+		CalculatorThreeNumbers addiction = (a, b, c) -> a + b + c;
+		CalculatorThreeNumbers subtract = (a, b, c) -> (a - b)- c;
+		CalculatorTwoNumbers multiply = (a, b) -> (a * b) ;
+		CalculatorTwoNumbers division = (a, b)  -> (a / ((b == 0)? 1:b) ); 
 		
 		System.out.println("suma -> "+addiction.operations(5, 5, 6));
 		System.out.println("resta -> "+subtract.operations(5, 6, 6));
-		System.out.println("multiplicacion -> "+multiply.operations(5, 5, 6.9f));
-		System.out.println("division -> "+division.operations(5, 5, 0));
+		System.out.println("multiplicacion -> "+multiply.operations(5, 6.9f));
+		System.out.println("division -> "+ String.format("%.2f", division.operations(5, 8)));
 	
-		
-		
-		System.out.println("division -> "+ String.format("%.2f", division.operations(5, 5, 6)));     //+division.operations(5, 5, 6));
-		
-		
-		
-		
-		
-		
+			
 
 	}
 	
 	@FunctionalInterface
-	interface Calculator{
-		float operations(float a, float b, float c);
+	interface CalculatorTwoNumbers{
+		float operations(float a, float b);
 		
+		
+	}
+	@FunctionalInterface
+	interface CalculatorThreeNumbers{
+		float operations(float a, float b, float c);
 	}
 	
 	
